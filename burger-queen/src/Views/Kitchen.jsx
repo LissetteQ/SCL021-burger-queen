53b66c64e5a1bc456258fd12c3ad601ab47b027d
LissetteQ <lissetteq123@gmail.com>
2022-11-18 13:19:34 -0300

    se ve el titulo del detalle de pedido en cocina

--- burger-queen/src/Views/Kitchen.jsx
+++ burger-queen/src/Views/Kitchen.jsx
@@ -6,7 +6,7 @@ import { traeData } from "../Firabase/Firestore.js";
 //hasta aqui se ve el pedido confirmado pero solo en consola, se debe trabajar para mostrarlo 
 //en la vista de cocina
 const Kitchen = () => {
-  const [otroEstado, setotroEstado] = useState([""]);
+  const [otroEstado, setotroEstado] = useState([]);
 
   useEffect(() => {
    async function getData(){
@@ -23,8 +23,9 @@ setotroEstado(data)
         <div className="carrito p-5">
           <div className="detalles bg-light w-75 m-auto p-5">
             <h5>Detalles del pedido:</h5>
+             
             <div className="p3 bg-white">
-              {otroEstado.map()}
+           
             </div>
           </div>
         </div>
