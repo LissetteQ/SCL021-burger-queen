f9bd3103f965ec9421f547d3e0bf57cfb68ce0a3
LissetteQ <lissetteq123@gmail.com>
2022-11-22 14:36:58 -0300

    se ven los pedidos en la vista de la cocina

--- burger-queen/src/Views/Kitchen.jsx
+++ burger-queen/src/Views/Kitchen.jsx
@@ -3,39 +3,48 @@ import { Container } from "react-bootstrap";
 import logo from "../Assets/img/logo.jpeg";
 import { traeData } from "../Firabase/Firestore.js";
 
-//hasta aqui se ve el pedido confirmado pero solo en consola, se debe trabajar para mostrarlo 
+//hasta aqui se ve el pedido confirmado pero solo en consola, se debe trabajar para mostrarlo
 //en la vista de cocina
 const Kitchen = () => {
-  const [otroEstado, setotroEstado] = useState([]);
+  const [newOrder, setnewOrder] = useState([]);
 
   useEffect(() => {
-   async function getData(){
-const data = await traeData()
-console.log(data)
-setotroEstado(data)
-   } 
-   getData()
+    async function getData() {
+      const data = await traeData();
+      console.log(data);
+      setnewOrder(data);
+    }
+    getData();
   }, []);
   return (
-<>
+    <>
       <Container>
-        <img className="logoCocina" src={logo} alt="" /> <div className="pt-4"></div>
+        <img className="logoCocina" src={logo} alt="" />{" "}
+        <div className="pt-4"></div>
         <div className="carrito p-5">
           <div className="detalles bg-light w-75 m-auto p-5">
             <h5>Detalles del pedido:</h5>
-             
+
             <div className="p3 bg-white">
-           
+              {newOrder.map((order, index) => (
+                <div>
+                  <h6>Orden {index + 1}</h6>
+                  <table>
+                    {order.carrito.map((item) => (
+                      <tr>
+                        <td>{item.name}</td>
+                        <td>{item.price}</td>
+                      </tr>
+                    ))}
+                  </table>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </Container>
-    
-        
-      
     </>
   );
 };
-   
 
 export default Kitchen;
